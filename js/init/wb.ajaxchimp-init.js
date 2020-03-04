2f7a13053c45160986f968bc1987f7663d5a69cf
rahuldev39 <rahuldev.w@gmail.com>
2020-03-04 15:18:43 +0530

    print and bookmark option in judgemnet detail page

--- js/init/wb.ajaxchimp-init.js
+++ js/init/wb.ajaxchimp-init.js
@@ -9,7 +9,7 @@
 $(function () {
   'use strict';
 
-  var $mailchimpURL = 'http://worksofwisnu.us6.list-manage.com/subscribe/post?u=b57b4e6ae38c92ac22d92a234&amp;id=17754c49aa'; // Replace the URL with your mailchimp URL (put your URL inside '').
+  var $mailchimpURL = '#'; // Replace the URL with your mailchimp URL (put your URL inside '').
 
   // callback function when the form submitted, show the notification box
 	function mailchimpCallback(resp) {
